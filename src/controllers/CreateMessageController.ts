47b6917c5a814dcd9e76cd6f010447b92e7c3a14
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-10-21 15:24:25 -0300

    Create a route to send a message

---
+++ src/controllers/CreateMessageController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { CreateMessageService } from "../services/CreateMessageService";
+
+class CreateMessageController {
+  async handle(req: Request, res: Response) {
+    const { message } = req.body;
+    const service = new CreateMessageService();
+
+    try {
+      const result = await service.execute(message, req.user_id);
+
+      return res.json(result);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+  }
+}
+
+export { CreateMessageController };
